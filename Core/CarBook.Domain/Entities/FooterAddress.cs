910d7034bc16c7680d716f218cc1c6275f3e6096
Sametcan Al <sametcanal53@hotmail.com>
2024-06-01 00:29:40 +0300

    Add project files.

---
+++ Core/CarBook.Domain/Entities/FooterAddress.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBook.Domain.Entities
+{
+    public class FooterAddress
+    {
+        public int FooterAddressID { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
